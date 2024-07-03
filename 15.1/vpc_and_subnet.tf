738b021e20cb69bbbf21daad468e28a0493dc217
Dmitry <morgotq@gmail.com>
2024-07-03 15:59:01 +0000

    Add VPC, subnets and several vm

---
+++ 15.1/vpc_and_subnet.tf
@@ -0,0 +1,18 @@
+resource "yandex_vpc_network" "my_vpc" {
+  name = "my_vpc"
+}
+
+resource "yandex_vpc_subnet" "subnet_public" {
+  name           = var.vpc_network.public.name
+  v4_cidr_blocks = var.vpc_network.public.cidr
+  zone           = var.vpc_network.public.zone
+  network_id     = yandex_vpc_network.my_vpc.id
+}
+
+resource "yandex_vpc_subnet" "subnet_private" {
+  name           = var.vpc_network.private.name
+  v4_cidr_blocks = var.vpc_network.private.cidr
+  zone           = var.vpc_network.private.zone
+  network_id     = yandex_vpc_network.my_vpc.id
+  route_table_id = yandex_vpc_route_table.private-route.id
+}
